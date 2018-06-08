711535b236e77b342381c6b7b9d6640ac20979e4
Anuj Bhati <anujbhati2008@gmail.com>
2018-06-08 19:50:54 -0400

    Initial commit

---
+++ CalorieCounter.cs
@@ -0,0 +1,48 @@
+﻿// Group Number  : 5
+// Assignment : Calorie Counter
+// Group Members : Anuj Bhati, Lucas Kaehler, Harikrushn Dave, Rohit sehgal
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calorie_Counter
+{
+    class CalorieCounter : INotifyPropertyChanged
+    {
+        public int fruitCalorie;
+        private double calorie = 0;
+        public double TotalCalorie
+        {
+            get { return calorie; }
+            set { calorie = value; OnPropertyChanged(); }
+
+        }
+        public void CountCalorie(int fruitCalorie)
+        {
+            this.fruitCalorie= fruitCalorie;
+            TotalCalorie += fruitCalorie;
+        }
+
+        public void ResetCalorie()
+        {
+            TotalCalorie = 0;
+        }
+
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿// Group Number  : 5
+// Assignment : Calorie Counter
+// Group Members : Anuj Bhati, Lucas Kaehler, Harikrushn Dave, Rohit Sehgal
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calorie_Counter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CalorieCounter count;
+        public MainWindow()
+        {
+            InitializeComponent();
+            count = new CalorieCounter();
+            DataContext = count;
+        }
+
+        private void btnApple_Click(object sender, RoutedEventArgs e)
+        {
+            count.CountCalorie(80);
+        }
+
+        private void btnBanana_Click(object sender, RoutedEventArgs e)
+        {
+            count.CountCalorie(115);
+        }
+
+        private void btnOrange_Click(object sender, RoutedEventArgs e)
+        {
+            count.CountCalorie(90);
+        }
+
+        private void btnPear_Click(object sender, RoutedEventArgs e)
+        {
+            count.CountCalorie(120);
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            count.ResetCalorie();
+        }
+    }
+}
